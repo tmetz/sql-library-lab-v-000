895e579b55d4c0f6dd5f850d85a1f91e31a0520e
Tammy Metz <tammy@tammymetz.com>
2018-09-18 23:08:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT title FROM Series
+  INNER JOIN Characters
+  ON Series.id = Characters.series_id
+  WHERE species = "human";"
 end
 
 def select_character_names_and_number_of_books_they_are_in
