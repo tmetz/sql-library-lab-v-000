0645e826eee8005e23b04d6a250a2586e55ea400
Tammy Metz <tammy@tammymetz.com>
2018-09-18 23:23:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT Characters.name, COUNT(Books.id) FROM Characters
+  "SELECT Characters.name, COUNT(character_books.book_id) FROM Characters
   INNER JOIN character_books
   ON Characters.id = character_books.character_id
   INNER JOIN Books
