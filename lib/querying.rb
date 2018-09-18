325a37d9f218eab345dc995d1a79bb25893729d6
Tammy Metz <tammy@tammymetz.com>
2018-09-18 23:10:59 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,8 @@ def select_series_title_with_most_human_characters
   "SELECT title FROM Series
   INNER JOIN Characters
   ON Series.id = Characters.series_id
-  WHERE Characters.species = 'human';"
+  GROUP BY Characters.species
+  HAVING Characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
