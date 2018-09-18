908154fa800f88673d88abe2824c39517f353e63
Tammy Metz <tammy@tammymetz.com>
2018-09-18 23:02:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) FROM Characters GROUP BY species DESC LIMIT 1;"
+  "SELECT species, COUNT(species) FROM Characters GROUP BY species ORDER BY species DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
